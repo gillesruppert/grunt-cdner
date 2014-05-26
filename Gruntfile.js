5f0df1c1b8d2b17be1ea0edc69625ad6016a2657
Gilles Ruppert <gilles@madeofbytes.com>
2014-05-26 17:07:29 +0200

    start adapting the Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,16 +16,16 @@ module.exports = function(grunt) {
       all: [
         'Gruntfile.js',
         'tasks/*.js',
-        '<%= nodeunit.tests %>'
+        '<%= nodeunit.tests %>',
       ],
       options: {
-        jshintrc: '.jshintrc'
-      }
+        jshintrc: '.jshintrc',
+      },
     },
 
     // Before generating any new files, remove any previously-created files.
     clean: {
-      tests: ['tmp']
+      tests: ['tmp'],
     },
 
     // Configuration to be run (and then tested).
@@ -34,24 +34,29 @@ module.exports = function(grunt) {
         options: {
         },
         files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
+          'tmp/default_options.css': ['test/fixtures/images.css'],
+          'tmp/default_options.html': ['test/fixtures/index.html'],
+        },
       },
-      custom_options: {
+      ignore_option: {
         options: {
-          separator: ': ',
-          punctuation: ' !!!'
+          buster: '0.2.0',
+          ignore: [
+            'cdn.1.test.com',
+            '//cdn.2.test.com',
+          ]
         },
         files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+          'tmp/ignore_option.css': ['test/fixtures/images.css'],
+          'tmp/ignore_option.html': ['test/fixtures/index.html'],
         }
       }
     },
 
     // Unit tests.
     nodeunit: {
-      tests: ['test/*_test.js']
-    }
+      tests: ['test/*_test.js'],
+    },
 
   });
 
